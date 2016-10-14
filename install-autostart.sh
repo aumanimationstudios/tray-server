01cd72176b3c91159d24c63e1c264cf16a9fbb2d
shrinidhi <shrinidhi666@gmail.com>
2016-10-14 14:40:42 +0530

    fixed a bug which displays msges twice

---
+++ install-autostart.sh
@@ -0,0 +1,1 @@
+cat /etc/autofs/auto.home | grep -i stor3: | gawk '{print "rsync -av /etc/xdg/autostart/tray-server.py.desktop /home/"$1"/.config/autostart/ ; chown "$1":artist /home/"$1"/.config/autostart -Rv"}' | sh -v
