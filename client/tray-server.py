45059d696f5d2773067f99dd5dfb42d82df32332
sanath111 <sanathshetty111@gmail.com>
2024-11-13 10:08:17 +0530

    fixes

--- client/tray-server.py
+++ client/tray-server.py
@@ -577,7 +577,6 @@ def rbhus_notify(scroll_ui,*args):
     else:
       checked = False
 
-    # if(not rbhus_notify_ids.has_key(x['id'])):
     if x['id'] not in rbhus_notify_ids:
       msg_box = uic.loadUi(textBox_ui_file)
       msg_box.setParent(scroll_ui)
